add56200f735f5bbbd88a9c2c2320f6b82a7a477
glokesh94 <glokesh94@gmail.com>
2019-09-25 14:49:58 +0530

    Updates

---
+++ precache-manifest.5ca4a70ac9803cfe32a48cb52d681980.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be47a59b748ca12e035ece8a1b6d4a96",
+    "url": "/Simple-Calculator-In-ReactJS/index.html"
+  },
+  {
+    "revision": "58e3861956b6dcf89c5b",
+    "url": "/Simple-Calculator-In-ReactJS/static/css/2.f87fbcaa.chunk.css"
+  },
+  {
+    "revision": "cbcec306351c4f1a1ec2",
+    "url": "/Simple-Calculator-In-ReactJS/static/css/main.5be8defa.chunk.css"
+  },
+  {
+    "revision": "58e3861956b6dcf89c5b",
+    "url": "/Simple-Calculator-In-ReactJS/static/js/2.9067ee69.chunk.js"
+  },
+  {
+    "revision": "cbcec306351c4f1a1ec2",
+    "url": "/Simple-Calculator-In-ReactJS/static/js/main.13a383d0.chunk.js"
+  },
+  {
+    "revision": "b5435fc1596a1087c714",
+    "url": "/Simple-Calculator-In-ReactJS/static/js/runtime~main.c9f30636.js"
+  }
+]);
